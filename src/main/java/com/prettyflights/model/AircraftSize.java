2e47922fa6666b89c21ab91eb9a90f6bdf186c9e
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2026-06-15 14:13:30 -0300

    Delete src/main/java/com/prettyflights/model directory

--- src/main/java/com/prettyflights/model/AircraftSize.java
+++
@@ -1,7 +0,0 @@
-package com.prettyflights.model.enums;
-
-public enum AircraftSize {
-    REGIONAL,    // até 80 passageiros
-    NARROW_BODY, // 80-200 passageiros (ex: Airbus A320, Boeing 737)
-    WIDE_BODY    // 200+ passageiros (ex: Boeing 777, Airbus A330)
-}
