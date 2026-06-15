1ed56ff293b426885dd6ac250b70ee192b61230f
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2026-06-15 14:14:38 -0300

    Add AircraftSize enum for aircraft classifications

---
+++ src/main/java/com/prettyflights/model/enums/AircraftSize.java
@@ -0,0 +1,7 @@
+package com.prettyflights.model.enums;
+
+public enum AircraftSize {
+    REGIONAL,    // Até 80 passageiros
+    NARROW_BODY, // 80-200 passageiros
+    WIDE_BODY    // 200+ passageiros
+}
